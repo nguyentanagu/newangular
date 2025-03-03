24abe0043f58cc6509a99ceba85abe6b1ea25230
nguyentan <nguyentan0842010534@gmail.com>
2025-03-03 11:40:09 +0700

    new angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from "./shared/header-layout/header-layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, HeaderLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
